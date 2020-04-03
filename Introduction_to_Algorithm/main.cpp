7d3738b784a114e2e252cc7afc58137c0c0494a0
Li Yanggeng <yli040@e.ntu.edu.sg>
2020-04-03 21:56:01 +0800

    Initial Commit

---
+++ Introduction_to_Algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Introduction_to_Algorithm
+//
+//  Created by Edith on 4/3/20.
+//  Copyright © 2020 Edith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
