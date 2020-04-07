09026db2c786b85ce389f57d72cbb10b5255029e
Li Yanggeng <yli040@e.ntu.edu.sg>
2020-04-07 19:16:03 +0800

    Add insertion sort algorithm

---
+++ Introduction_to_Algorithm/sort_methods.hpp
@@ -0,0 +1,19 @@
+//
+//  sort_methods.hpp
+//  Introduction_to_Algorithm
+//
+//  Created by Edith on 4/3/20.
+//  Copyright © 2020 Edith. All rights reserved.
+//
+
+#ifndef sort_methods_hpp
+#define sort_methods_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+
+std::vector<int> insertionSort(std::vector<int>& unsorted_array);
+
+#endif /* sort_methods_hpp */
+
