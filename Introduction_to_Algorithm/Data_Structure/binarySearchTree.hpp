87bd09beb5c9d4bc92e577416461fe8ecd8e0813
Li Yanggeng <yli040@e.ntu.edu.sg>
2020-06-12 19:18:02 +0800

    Add some features to the binary search tree

--- Introduction_to_Algorithm/Data_Structure/binarySearchTree.hpp
+++ Introduction_to_Algorithm/Data_Structure/binarySearchTree.hpp
@@ -23,17 +23,28 @@ public:
 	
 	const Comparable& findMin() const;
 	const Comparable& findMax() const;
-	bool contains(const Comparable& x) const;
+	
+	bool contains(const Comparable& x) const{
+		return contains(x, root);
+	};
+	
 	bool isEmpty() const;
 	void printTree(std::ostream& out = std::cout) const;
 	
 	void makeEmpty();
-	void insert(const Comparable& x);
+	void insert(const Comparable& x){
+		insert(x, root);
+	};
+	
 	void insert(Comparable&& x);
-	void remove(const Comparable& x);
+	void remove(const Comparable& x){
+		remove(x, root);
+	};
 	
 	BinarySearchTree& operator= (const BinarySearchTree& rhs);
 	BinarySearchTree& operator= (BinarySearchTree&& rhs);
+	
+	
 	
 private:
 	struct BinaryNode{
@@ -56,7 +67,18 @@ private:
 	
 	BinaryNode* findMin(BinaryNode* t) const;
 	BinaryNode* findMax(BinaryNode* t) const;
-	bool contains(const Comparable& x, const BinaryNode* t) const;
+	
+	bool contains(const Comparable& x, const BinaryNode* t) const{
+		if(t == nullptr) return false;
+		else if(x < t->element)
+			return contains(x, t->left);
+		else if(x > t->element)
+			return contains(x, t->right);
+		else
+			return true;
+	};
+
+	
 	void makeEmpty(BinaryNode* &t);
 	void printTree(BinaryNode* t, std::ostream& out) const;
 	BinaryNode* clone(BinaryNode* t) const;
